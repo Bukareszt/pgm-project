294c7fade2ab7693f1f2f1e80edd0ae5317be259
refrigerr <79880735+refrigerr@users.noreply.github.com>
2024-06-18 20:41:21 +0200

    skopiowane z listy ez

---
+++ naive_bayes.py
@@ -0,0 +1,116 @@
+import pyro
+from pyro import distributions as dist
+import torch
+from torch.distributions import constraints
+
+pyro.enable_validation(True)
+
+import pyro
+import torch
+from tqdm.auto import tqdm
+
+
+class GaussianNBClassifier:
+    def __init__(self, num_epochs=500, lr=1e-2):
+        self._num_epochs = num_epochs
+        self._lr = lr
+        
+        self._num_cls = None
+        
+        self._c_logits = None        
+        self._num_probs = None
+        
+    def fit(self, X, y):
+        pyro.clear_param_store()
+
+        svi = pyro.infer.SVI(
+            model=self._model,
+            guide=self._guide,
+            optim=pyro.optim.Adam({'lr': self._lr}),
+            loss=pyro.infer.Trace_ELBO(),
+        )
+
+        with tqdm(range(self._num_epochs)) as pbar:
+            for epoch in pbar:
+                loss = svi.step(X, y)
+
+                if epoch % 100 == 0:
+                    print(f"Epoch: {epoch} Loss = {loss:.3f}")
+
+    def predict(self, X):
+        pred = pyro.infer.Predictive(
+            model=self._model,
+            guide=self._guide,
+            num_samples=1,
+            return_sites=('logP(c|x)',),
+        )
+        log_pcx = pred(X)['logP(c|x)'].detach().squeeze(0).squeeze(0)
+        y_pred = torch.argmax(log_pcx, dim=-1)
+        return y_pred
+    
+    def _model(self, X, y=None):    
+        if y is not None:  # training mode
+            self._num_cls = max(y) + 1
+            
+            numerical_cols = X.columns.values
+                    
+            self._init_c_logits()
+            self._init_num_params(X, numerical_cols)
+            self._observe_numerical_features_given_classes(X, y)
+        else:
+            self._get_classes_log_probs(X)
+        
+    def _guide(self, X, y=None):
+        pass  # This is meant to be an empty function
+    
+    def _init_c_logits(self):
+        self._c_probs = pyro.param(
+            'c_probs',
+            lambda: torch.ones(self._num_cls).div(self._num_cls),
+            constraint=constraints.simplex,
+        )
+        
+    def _init_num_params(self, X, numerical_cols):
+        self._num_dists = {
+            col: {
+                'mu': pyro.param(f'{col}_mu', lambda: torch.zeros(self._num_cls)),
+                'sigma': pyro.param(
+                    f'{col}_sigma',
+                    lambda: torch.ones(self._num_cls),
+                    constraint=constraints.positive,
+                ),
+            }
+            for col in numerical_cols
+        }
+        
+    def _observe_numerical_features_given_classes(self, X, y):
+        for c in range(self._num_cls):
+            x_c = X[y==c]
+            with pyro.plate(f'data-numerical-{c}', x_c.shape[0]):
+                for nc, v in self._num_dists.items():
+                    pyro.sample(
+                        f'x_{nc}|c={c}', 
+                        dist.Normal(v['mu'][c], v['sigma'][c]),
+                        obs=torch.tensor(x_c[nc].values),
+                    )
+                    
+    def _get_log_likelihood(self, X):
+        log_lk = []
+        
+        for c in range(self._num_cls):
+            lps = []
+            
+            lps.extend([
+                dist.Normal(v['mu'][c], v['sigma'][c]).log_prob(torch.tensor(X[nc].values))
+                for nc, v in self._num_dists.items()
+            ])
+
+            log_lk.append(torch.stack(lps).sum(dim=0))
+            
+        return torch.stack(log_lk).t()
+    
+    def _get_classes_log_probs(self, X):
+        
+        log_lk = self._get_log_likelihood(X)
+
+        log_pcx = pyro.deterministic('logP(c|x)', self._c_probs.log() + log_lk)
